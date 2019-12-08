1c00b9de01394966abac88959098d3e1145cc7b6
Mark Oveson <mark.oveson@mx.com>
2019-12-08 11:40:37 -0700

    Use correct argument order for asserts

--- test/fuel_test.rb
+++ test/fuel_test.rb
@@ -4,19 +4,19 @@ require_relative "../lib/fuel"
 describe Fuel do
   describe ".calculate" do
     it "calculates fuel requirements for a small mass" do
-      assert_equal Fuel.calculate(6), 0
+      assert_equal 0, Fuel.calculate(6)
     end
 
     it "calculates fuel requirements for a larger mass" do
-      assert_equal Fuel.calculate(10), 1
+      assert_equal 1, Fuel.calculate(10)
     end
 
     it "calculates fuel requirements for a much larger mass" do
-      assert_equal Fuel.calculate(1969), 966
+      assert_equal 966, Fuel.calculate(1969)
     end
 
     it "calculates fuel requirements for a very large mass" do
-      assert_equal Fuel.calculate(100756), 50346
+      assert_equal 50346, Fuel.calculate(100756)
     end
   end
 end
