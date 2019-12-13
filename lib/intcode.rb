48e12e408c172f157f9fbe7d90b4b1355c8d6537
Mark Oveson <mark.oveson@mx.com>
2019-12-12 21:39:55 -0700

    Intcode class

--- lib/intcode.rb
+++ lib/intcode.rb
@@ -1,4 +1,22 @@
 class Intcode
-  def self.execute(integers)
+  def self.execute(intcode)
+    i = 0
+    done = false
+
+    until done do
+      case intcode[i]
+      when 1
+        intcode[intcode[i + 3]] = intcode[intcode[i + 1]] + intcode[intcode[i + 2]]
+      when 2
+        intcode[intcode[i + 3]] = intcode[intcode[i + 1]] * intcode[intcode[i + 2]]
+      when 99
+        done = true
+      else
+        raise ArgumentError "Unknown intcode: #{intcode[i]}"
+      end
+      i += 4
+    end
+
+    intcode
   end
 end
